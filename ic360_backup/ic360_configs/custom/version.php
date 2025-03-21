e10f82780279546bc3f037c833d6a858c55d1ba9
DeepakNarayanan <dnarayanan@ic360.io>
2025-03-21 11:38:32 -0400

    USIPRJ-3087-cleaning up the folder to redo the fork with ricky's recommendation

--- ic360_backup/ic360_configs/custom/version.php
+++
@@ -1,8 +0,0 @@
-<?php
-defined('MOODLE_INTERNAL') || die();
-
-$plugin->version = 2024030301;  // Increment this number
-$plugin->requires = 2020061500;
-$plugin->component = 'local_customlog';
-$plugin->maturity = MATURITY_STABLE;
-$plugin->release = '1.0';
